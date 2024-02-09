ab5274d26dd9814ef69a0cd8f5841fb8f2b5e843
Ryan <ryanpinas77@gmail.com>
2024-02-09 16:45:26 +0100

    update homepage

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import background from './images/background.jpg'; // Importez l'image de fond
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bienvenue sur HairSwift.
-        </p>
-      </header>
+    <div className="App" style={{backgroundImage: `url(${background})`, backgroundSize: "cover", backgroundPosition: "center", backgroundAttachment: "fixed", height: "100vh", textAlign: "center", display: "flex", flexDirection: "column", justifyContent: "center"}}>
+      <div className="overlay shadow-xl">
+      <h1 style={{fontWeight: "bold", fontSize: "48px", color: "white"}} >Bienvenue sur HairSwift</h1>
+        <h2 style={{fontSize: "28px", color: "white"}}>Choisissez votre coiffeur</h2>
+        <h2 style={{fontSize: "24px", color: "white"}}>Rapide - Simple - Disponible 24h/24</h2>
+        <input type="text" placeholder="Rechercher un salon de coiffure..." className="search-bar" />
+      </div>
     </div>
   );
 }
