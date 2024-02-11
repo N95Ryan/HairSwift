1aadedbe357a08d3c55e165e399aa230533a4029
Ryan <ryanpinas77@gmail.com>
2024-02-11 12:44:45 +0100

    salon added

--- front/src/components/overlay/overlay.js
+++ front/src/components/overlay/overlay.js
@@ -4,8 +4,9 @@ function Overlay() {
   return (
     <>
       <div className="overlay">
-      <img src={logo} className="logo animate-jump-in" alt="Logo" />      
-      <span className="text-2xl text-white font-light my-4 animate-fade-up ">
+      <a href="/">
+        <img src={logo} className="logo animate-jump-in" alt="Logo" />
+      </a>      <span className="text-2xl text-white font-light my-4 animate-fade-up ">
           Rapide • Efficace • Disponible 24h/24
         </span>
         <div className="flex items-center justify-center animate-fade-up">
